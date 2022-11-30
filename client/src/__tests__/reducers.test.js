30e5899a54f4dd3172084ac2ed75738c09aee3a6
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-11-30 11:58:02 -0500

    user can add to cart, add multiple to cart and remove from cart

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -3,13 +3,33 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "../utils/actions";
 import { reducer } from "../utils/reducers";
 const initialState = {
   products: [],
   categories: [{ name: "Food" }],
   currentCategory: "1",
+  cart: [
+    {
+      _id: "1",
+      name: "Soup",
+      purchaseQuantity: 1,
+    },
+    {
+      _id: "2",
+      name: "Bread",
+      purchaseQuantity: 2,
+    },
+  ],
+  cartOpen: false,
 };
+// we always check that something has been updated when action occurs, and check that our initial state was not updated (immutable!)
 
 test("UPDATE_PRODUCTS", () => {
   let newState = reducer(initialState, {
@@ -39,4 +59,48 @@ test("UPDATE_CURRENT_CATEGORY", () => {
 
   expect(newState.currentCategory).toBe("2");
   expect(initialState.currentCategory).toBe("1");
+});
+
+test("ADD_TO_CART", () => {
+  let newState = reducer(initialState, {
+    type: ADD_TO_CART,
+    product: { purchaseQuantity: 1 },
+  });
+
+  expect(newState.cart.length).toBe(3);
+  expect(initialState.cart.length).toBe(2);
+});
+test("ADD_MULTIPLE_TO_CART", () => {
+  let newState = reducer(initialState, {
+    type: ADD_MULTIPLE_TO_CART,
+    products: [{}, {}],
+  });
+
+  expect(newState.cart.length).toBe(4);
+  expect(initialState.cart.length).toBe(2);
+});
+test("REMOVE_FROM_CART", () => {
+  let newStateOne = reducer(initialState, {
+    type: REMOVE_FROM_CART,
+    _id: "1",
+  });
+
+  // cart is still open
+  expect(newStateOne.cartOpen).toBe(true);
+
+  // second item should now be first
+  expect(newStateOne.cart.length).toBe(1);
+  expect(newStateOne.cart[0]._id).toBe("2");
+
+  let newStateTwo = reducer(newStateOne, {
+    type: REMOVE_FROM_CART,
+    _id: "2",
+  });
+
+  // cart is empty and closed
+
+  expect(newStateTwo.cartOpen).toBe(false);
+  expect(newStateTwo.cart.length).toBe(0);
+
+  expect(initialState.cart.length).toBe(2);
 });
