2e30f2337b38a631bca49451e5efded0f49437e4
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-02 07:32:32 -0500

    added functional use for cart, adding, updating and removing items while updating price and quantity

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,33 @@
 import React from "react";
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
 const CartItem = ({ item }) => {
+  // no need to read state
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+  };
+  const onInputChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "0") {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
+  };
   return (
     <div className="flex-row">
       <div>
@@ -12,8 +39,17 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} />
-          <span role="img" aria-label="trash">
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onInputChange}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
             🗑️
           </span>
         </div>
